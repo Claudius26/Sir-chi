1e62168401adaa8f66d8e68ec59b8eb5b1d647c3
claudius26 <osomzed@gmail.com>
2024-12-06 23:03:13 -0800

    add files

---
+++ EvenNumbers.java
@@ -0,0 +1,13 @@
+public class EvenNumbers {
+
+	public static void main(String[] args){
+
+for(int number = 1; number < 11; number++) {
+
+	if (number % 2 == 0) System.out.println(number);
+
+
+}
+
+}
+}
